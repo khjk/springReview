b45c05cd6134bd75e70499b2a49719221be4d978
khjk <begywjd@naver.com>
2020-07-28 10:26:49 +0900

    usage of "annotation config application context"

--- src/main/java/chap02/AppContext.java
+++ src/main/java/chap02/AppContext.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class AppContext {
     @Bean
     public Greeter greeter(){
-        Greeter g = new Greeter();
-        g.setFormat("%s, 안녕하세요");
-        return g;
+        Greeter greeter = new Greeter();
+        greeter.setFormat("%s, 안녕하세요");
+        return greeter;
     }
 }
